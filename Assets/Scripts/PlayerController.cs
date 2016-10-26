535c3d0aeb8b597091c1efee41841d949f8549f4
Oni0nKnight <theophile.gindre@gmail.com>
2016-10-26 18:08:08 -0400

    ninja sprites + basic animations

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,35 +6,68 @@ public class PlayerController : MonoBehaviour {
     public float speed;
     public GameObject shot;
     public float fireRate;
+	public int life;
     private float fireTimer;
+
+	private Animator anim;
 
 	// Use this for initialization
 	void Start () {
+		anim = GetComponent<Animator>();
+
         fireTimer = fireRate;
+		life = 100;
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate () {
+		if (!isDead ()) {
+			fireTimer = Mathf.Max (fireTimer - Time.fixedDeltaTime, 0f);
 
-        fireTimer -= Time.fixedDeltaTime;
+			// Movement routine
+			float moveHorizontal = Input.GetAxis ("Horizontal");
+			float moveVertical = Input.GetAxis ("Vertical");
+			Vector2 movement = new Vector2 (moveHorizontal, moveVertical);
+			GetComponent<Rigidbody2D> ().velocity = movement * speed;
 
-        // Movement routine
-        float moveHorizontal = Input.GetAxis("Horizontal");
-        float moveVertical = Input.GetAxis("Vertical");
-        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
-        GetComponent<Rigidbody2D>().velocity = movement * speed;
-
-        // Shooting routine
-        float shotHorizontal = Input.GetAxis("HorizontalShot");
-        float shotVertical = Input.GetAxis("VerticalShot");
-        Vector2 direction = new Vector2(shotHorizontal, shotVertical);
-        direction.Normalize();
-        if(direction != Vector2.zero && fireTimer <= 0)
-        {
-            GameObject shotFired = (GameObject)Instantiate(shot, transform.position, transform.rotation);
-            shotFired.GetComponent<Mover>().direction = direction;
-            fireTimer = fireRate;
-        }
-        
+			// Shooting routine
+			if (Input.GetAxis ("HorizontalShot") != 0 || Input.GetAxis ("VerticalShot") != 0)
+				shotBullet ();
+		}
     }
+
+	private void shotBullet()
+	{
+		float shotHorizontal = Input.GetAxis("HorizontalShot");
+		float shotVertical = Input.GetAxis("VerticalShot");
+		Vector2 direction = new Vector2(shotHorizontal, shotVertical);
+		direction.Normalize();
+
+		if (direction != Vector2.zero && fireTimer <= 0) {
+			GameObject shotFired = (GameObject)Instantiate (shot, transform.position, transform.rotation);
+			shotFired.GetComponent<Mover> ().direction = direction;
+			fireTimer = fireRate;
+
+			//déclenchement de l'animation de tir
+			anim.SetTrigger ("fire");
+		}
+	}
+
+	private void takeDamage(int damage)
+	{
+		if (!isDead()) {
+			life = Mathf.Max (life - damage, 0);
+
+			if (isDead()) { //si ce dégât vient de nous tuer
+				GetComponent<Rigidbody2D> ().velocity = Vector2.zero; //on arrête de bouger
+				//animation de la mort
+				anim.SetTrigger ("death");
+			}
+		}
+	}
+
+	private bool isDead()
+	{
+		return life <= 0;
+	}
 }
