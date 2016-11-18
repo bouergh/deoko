fd681a6f9ba0a270c545474ba8c824347ac18a57
Arthur Fournier <arfoub@gigl.polymtl.ca>
2016-11-18 17:59:04 -0500

    avancee sur projectiles et tazman fait sur autre pc

---
+++ Assets/Scripts/Projectiles/BulletController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletController : ProjectileController{
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        GetComponent<Rigidbody2D>().velocity = direction * speed;   //projectile goes towards its initial direction, at same speed, for the moment
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (!other.isTrigger && other.tag == "Enemy" && origin != "Enemy")  //enemy has trigger collider for range detection so we only check non-trigger colliders
+        {
+            other.GetComponent<EnemyController>().TakeDamage(damage);
+            Destroy(this.gameObject);
+        }
+
+        if (other.tag == "Player" && origin != "Player")
+        {
+            other.GetComponent<PlayerController>().TakeDamage(damage);
+            Destroy(this.gameObject);
+        }
+
+        if (other.tag == "Wall")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
