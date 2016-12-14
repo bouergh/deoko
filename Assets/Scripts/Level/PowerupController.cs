adc039256294f2fada35a5d8b1d85ccc2e020eaa
bouergh <voodbutcher@gmail.com>
2016-12-14 17:02:03 -0500

    pickup keys and powerup, open locks, fixed character size and levelgenerator

---
+++ Assets/Scripts/Level/PowerupController.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PowerupController : MonoBehaviour {
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            other.GetComponent<PlayerController>().ChangePowerup(1);
+            Destroy(gameObject);
+        }
+    }
+}
