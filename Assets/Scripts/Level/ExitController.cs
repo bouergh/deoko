3ac57a26c60e5bb76a878c1236d76dab061a0bec
bouergh <voodbutcher@gmail.com>
2016-12-14 17:30:40 -0500

    start and end screen, scenes loading

--- Assets/Scripts/Level/ExitController.cs
+++ Assets/Scripts/Level/ExitController.cs
@@ -1,15 +1,14 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.SceneManagement;
 
 public class ExitController : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+	void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "Player")
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1);
+        }
+    }
 }
