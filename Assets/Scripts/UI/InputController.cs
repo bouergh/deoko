d4197caa5c7c86b9fb4fd407831e997701b73361
Oni0nKnight <theophile.gindre@gmail.com>
2016-12-16 15:28:46 -0500

    death menu improved (R and M keys)

--- Assets/Scripts/UI/InputController.cs
+++ Assets/Scripts/UI/InputController.cs
@@ -2,9 +2,19 @@
 using System.Collections;
 
 public class InputController : MonoBehaviour {
+	private GameManager gameManager;
 
-	// Update is called once per frame
+
+	void Awake() {
+		gameManager = GameObject.Find ("BoardManager").GetComponent<GameManager> ();
+	}
+
 	void Update () {
-		
+		if (Input.GetButton ("Restart")) {
+			gameManager.RestartGame ();
+		}
+		if (Input.GetButton ("MainMenu")) {
+			gameManager.MainMenu ();
+		}
 	}
 }
